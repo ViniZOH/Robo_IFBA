d7f84f2adc0ff384a4a5a68ee92f89396b8ac032
Vinicius Barreto <viniciusbarretoeng0@gmail.com>
2025-01-21 10:44:20 -0300

    Destrinchado o if em 3 condiçoes

--- PID_Basic/PID_Basic.ino
+++ PID_Basic/PID_Basic.ino
@@ -31,27 +31,23 @@ Projeto de Sistemas Mecatrônicos
 #define MOTOR_2_MIN 135
 #define MOTOR_3_MAX 837
 #define MOTOR_3_MIN 125
-#define MOTOR_4_MAX 372
-#define MOTOR_4_MIN 125
+#define MOTOR_4_MAX 649
+#define MOTOR_4_MIN 371
 #define MOTOR_5_MAX 920
 #define MOTOR_5_MIN 93
 
-//Define Variables we'll be connecting to
-double Setpoint, Input, Output;
 
 double set_point_mot_1, set_point_mot_2, set_point_mot_3, set_point_mot_4, set_point_mot_5;
 double input_garra, input_mot_2, input_mot_3, input_mot_4, input_mot_5;
 double output_mot_1, output_mot_2, output_mot_3, output_mot_4, output_mot_5;
 
 // Criando objetos PID e linkando as variáveis
-double Kp=2, Ki=5, Kd=0;
+double Kp=20, Ki=3, Kd=3;
 PID myPID1(&input_garra, &output_mot_1, &set_point_mot_1, Kp, Ki, Kd, DIRECT);
 PID myPID2(&input_mot_2, &output_mot_2, &set_point_mot_2, Kp, Ki, Kd, DIRECT);
 PID myPID3(&input_mot_3, &output_mot_3, &set_point_mot_3, Kp, Ki, Kd, DIRECT);
 PID myPID4(&input_mot_4, &output_mot_4, &set_point_mot_4, Kp, Ki, Kd, DIRECT);
 PID myPID5(&input_mot_5, &output_mot_5, &set_point_mot_5, Kp, Ki, Kd, DIRECT);
-
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 void setup()
 {
@@ -71,53 +67,84 @@ void setup()
 
   // Inicializando as variáveis
   //input_mot_1 = analogRead(POT_1_MOT_2);
-  set_point_mot_2 = 512;
-  input_mot_2 = analogRead(POT_1_MOT_2);
+//  set_point_mot_2 = 512;
+//  input_mot_2 = analogRead(POT_1_MOT_2);
   set_point_mot_3 = 512;
   input_mot_3 = analogRead(POT_2_MOT_3);
-  set_point_mot_4 = 512;
-  input_mot_4 = analogRead(POT_1_MOT_2);
-  set_point_mot_5 = 512;
+  set_point_mot_4 = 400;
+  input_mot_4 = analogRead(POT_3_MOT_4);
+//  set_point_mot_5 = 512;
 //  input_mot_1 = analogRead(POT_1_MOT_2);
-  set_point_mot_2 = 512;
+//  set_point_mot_2 = 512;
 
   // Ligando o PID
-  myPID1.SetMode(AUTOMATIC);
-  myPID2.SetMode(AUTOMATIC);
+//  myPID1.SetMode(AUTOMATIC);
+//  myPID2.SetMode(AUTOMATIC);
   myPID3.SetMode(AUTOMATIC);
   myPID4.SetMode(AUTOMATIC);
-  myPID5.SetMode(AUTOMATIC);
+//  myPID5.SetMode(AUTOMATIC);
 }
 
 void loop()
 {
   input_mot_3 = analogRead(POT_2_MOT_3);
   myPID3.Compute();
+  input_mot_4 = analogRead(POT_3_MOT_4);
+  myPID4.Compute();
 
   if (input_mot_3 > MOTOR_3_MIN && input_mot_3 < MOTOR_3_MAX)
   {
-    if (input_mot_3 > Setpoint)
+    if (input_mot_3 < set_point_mot_3)
     {
-      Serial.print("if        ");
-      Serial.println(input_mot_3);
+      Serial.print("if  input:   ");
+      Serial.print(input_mot_3);
+      Serial.print("   ||  output:   ");
+      Serial.println(output_mot_3);
       analogWrite(DRIVER_2_IN_1, 0);
       analogWrite(DRIVER_2_IN_2, output_mot_3);
     }
-    else
+    else if (input_mot_3 > set_point_mot_3)
     {
-      Serial.print("else       ");
-      Serial.println(input_mot_3);
+      Serial.print("else  input:   ");
+      Serial.print(input_mot_3);
+      Serial.print("   ||  output:   ");
+      Serial.println(output_mot_3);
       analogWrite(DRIVER_2_IN_1, output_mot_3);
       analogWrite(DRIVER_2_IN_2, 0);
     }
   }
-  else
+  else // Dentro do setpoint
   {
     digitalWrite(DRIVER_2_IN_1, LOW);
     digitalWrite(DRIVER_2_IN_2, LOW);
   }
-  // Serial.print("Pot 2: ");
-  // Serial.println(Input);
-  Serial.print("Output: ");
-  Serial.println(output_mot_3);
+
+  if (input_mot_4 > MOTOR_4_MIN && input_mot_4 < MOTOR_4_MAX)
+  {
+    if (input_mot_4 < set_point_mot_4)
+    {
+//      Serial.print("if  input:   ");
+//      Serial.print(input_mot_4);
+//      Serial.print("   ||  output:   ");
+//      Serial.println(output_mot_4);
+      analogWrite(DRIVER_2_IN_3, 0);
+      analogWrite(DRIVER_2_IN_4, output_mot_4);
+    }
+    else if (input_mot_4 > set_point_mot_4)
+    {
+//      Serial.print("else  input:   ");
+//      Serial.print(input_mot_4);
+//      Serial.print("   ||  output:   ");
+//      Serial.println(output_mot_4);
+      analogWrite(DRIVER_2_IN_3, output_mot_4);
+      analogWrite(DRIVER_2_IN_4, 0);
+    }
+  }
+  else
+  {
+    digitalWrite(DRIVER_2_IN_3, LOW);
+    digitalWrite(DRIVER_2_IN_4, LOW);
+  }
+
+  
 }
